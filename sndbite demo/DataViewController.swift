bcc3f81289c1d8d172532b877ce7b1b7061f3c2f
mariaprokopetz <mprokopetz92@yahoo.com>
2017-02-01 13:47:25 -0500

    Initial Commit

---
+++ sndbite demo/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  sndbite demo
+//
+//  Created by Maria Prokopetz on 2/1/17.
+//  Copyright © 2017 Maria Prokopetz. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
